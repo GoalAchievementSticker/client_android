e7ebc0beaa8f277e5c8d2d27496df7c4bc27fdde
김수빈 <77378559+subinkim26@users.noreply.github.com>
2022-05-17 19:39:47 +0900

    data notify

--- app/src/main/java/com/example/java_sticker/CustomAdapter.java
+++ app/src/main/java/com/example/java_sticker/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.example.java_sticker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +31,7 @@ public class CustomAdapter extends BaseAdapter {
     public CustomAdapter(Context context,ArrayList<GridItem> items) {
         this.context = context;
         this.items = items;
-    }
+     }
 
 
     @Override
@@ -58,9 +62,24 @@ public class CustomAdapter extends BaseAdapter {
             //sticker_img.setImageResource(gridItem.getGoal_img_Id());
             sticker_img = (TextView) convertView.findViewById(R.id.sticker_img);
             sticker_img.setText(gridItem.getTest());
+
+
+            int pos = position;
+            sticker_img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent intent3 = new Intent(view.getContext(),custom_p_goal_click.class);
+                    intent3.putExtra("pos",pos);
+                    //gridItem.setTest("무지개");
+                    //sticker_img.setText("무지개");
+                }
+            });
         }else{
             View view = new View(context);
             view = (View) convertView;
+
+
         }
 
 
--- app/src/main/java/com/example/java_sticker/Custom_p_item_adapter.java
+++ app/src/main/java/com/example/java_sticker/Custom_p_item_adapter.java
@@ -7,6 +7,7 @@ import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
